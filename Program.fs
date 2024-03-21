cf0782aa7d5aeebc092ef2787de403af6a037398
Mahib09 <Mahib.Ranamagar@triosstudent.com>
2024-03-21 14:07:31 -0400

    Step 1: Partial Application

--- Program.fs
+++ Program.fs
@@ -1,2 +1,18 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+//Step 1: Partial Application
+// Define a function that takes an exponent and a value and returns the result of applying the exponent to the value
+let power exponent value =
+    value ** exponent
+// Partially apply the function with exponent 2 to create a square function
+let square = power 2.0
+// Partially apply the function with exponent 3 to create a cube function
+let cube = power 3.0
+// Define a function to print the results of applying square and cube to a given value
+let printResults value =
+    printfn "Square of %.2f: %.2f" value (square value)
+    printfn "Cube of %.2f: %.2f" value (cube value)
+let functionResult = power 3.0 4.0
+printfn "Output of the power Function: %.2f" functionResult
+// Test the square and cube functions with different values
+let test = [2.0; 3.0; 4.0]
+List.iter printResults test
--- obj/Debug/net8.0/Lab1_CSD215_TailRecursion_Mahib.AssemblyInfo.fs
+++ obj/Debug/net8.0/Lab1_CSD215_TailRecursion_Mahib.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("Lab1_CSD215_TailRecursion_Mahib")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bff3216d29cffb0915d94e7839ff0b19a5a65bf")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("Lab1_CSD215_TailRecursion_Mahib")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("Lab1_CSD215_TailRecursion_Mahib")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
